a68462535610d18a3a1070fe66ce8bb5ddd91151
Toby Alden <toby.alden@gmail.com>
2017-12-07 21:14:43 -0800

    Initial commit (lol)

---
+++ source/FieldTile.hx
@@ -0,0 +1,15 @@
+
+package;
+
+import flixel.*;
+
+class FieldTile extends FlxSprite
+{
+    public function new(x:Int, y:Int) {
+        super(x, y);
+        loadGraphic('assets/images/ground.png', true, 32, 32);
+        animation.add('dry', [0]);
+        animation.add('wet', [1]);
+        animation.play('dry');
+    }
+}
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,24 @@
 package;
 
-import flixel.FlxState;
+import flixel.*;
 
 class PlayState extends FlxState
 {
-    private var player:Player;
+    public static inline var FIELD_SIZE = 10;
+    public static inline var TILE_SIZE = 32;
 
 	override public function create():Void
 	{
-        player = new Player(20, 20);
-        add(player);
 		super.create();
+        for(x in 0...FIELD_SIZE) {
+            for(y in 0...FIELD_SIZE) {
+                var tile = new FieldTile(x * TILE_SIZE, y * TILE_SIZE);
+                add(tile);
+            }
+        }
+        var grid = new FlxSprite(0, 0);
+        grid.loadGraphic('assets/images/grid.png');
+        add(grid);
 	}
 
 	override public function update(elapsed:Float):Void
