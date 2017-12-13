b7622dc57e4a83d2b7d3126bb89283c734db86e3
Toby Alden <toby.alden@gmail.com>
2017-12-12 18:27:45 -0800

    Add rest of till patterns

--- source/TillCard.hx
+++ source/TillCard.hx
@@ -27,11 +27,60 @@ class TillCard extends Card
                 [1, 0, 1]
             ]);
         }
+        else if(patternNum == 3) {
+            robot.till([
+                [1, 0, 1],
+                [1, 0, 1],
+                [1, 0, 1]
+            ]);
+        }
+        else if(patternNum == 4) {
+            robot.till([
+                [1, 1, 1],
+                [0, 0, 0],
+                [1, 1, 1]
+            ]);
+        }
         else if(patternNum == 5) {
             robot.till([
                 [1, 1, 1],
                 [0, 0, 1],
                 [0, 0, 1]
+            ]);
+        }
+        else if(patternNum == 6) {
+            robot.till([
+                [1, 1, 1],
+                [1, 0, 0],
+                [1, 0, 0]
+            ]);
+        }
+        else if(patternNum == 7) {
+            robot.till([
+                [1, 0, 1],
+                [1, 0, 1],
+                [0, 1, 0]
+            ]);
+        }
+        else if(patternNum == 8) {
+            robot.till([
+                [0, 1, 0],
+                [1, 0, 1],
+                [1, 0, 1]
+            ]);
+        }
+        else if(patternNum == 9) {
+            robot.till([
+                [1, 1, 1],
+                [1, 0, 1],
+                [0, 0, 0]
+            ]);
+        }
+        else if(patternNum == 10) {
+            robot.till([
+                [0, 0, 0],
+                [1, 0, 1],
+                [1, 1, 1]
             ]);
         }
         super.action();
