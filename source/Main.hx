a78f853024f28c9fea91d4c31ebeb032f3890568
Toby Alden <toby.alden@gmail.com>
2018-01-12 22:57:13 -0800

    Add mouse pointer

--- source/Main.hx
+++ source/Main.hx
@@ -9,10 +9,10 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, HighScores, 1, 60, 60, true));
+		addChild(new FlxGame(0, 0, PlayState, 1, 60, 60, true));
         FlxG.mouse.visible = true;
         var sprite = new FlxSprite();
-        sprite.makeGraphic(15, 15, FlxColor.WHITE);
-        FlxG.mouse.load(sprite.pixels);
+        sprite.loadGraphic('assets/images/pointer.png');
+        FlxG.mouse.load(sprite.pixels, 0.75, -20, -10);
 	}
 }
