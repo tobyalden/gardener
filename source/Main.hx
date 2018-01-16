0b2392f46f6c1e418dfa93d11e9c4d943ee0cf64
Toby Alden <toby.alden@gmail.com>
2018-01-15 19:13:15 -0800

    HaxeFlixel project nonsense

--- source/Main.hx
+++ source/Main.hx
@@ -9,10 +9,14 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, Diary, 1, 60, 60, true));
+        addChild(new FlxGame(0, 0, Diary, 1, 60, 60, true));
+        //addChild(new FlxGame(0, 0, HighScores, 1, 60, 60, true));
         FlxG.mouse.visible = true;
         var sprite = new FlxSprite();
         sprite.loadGraphic('assets/images/pointer.png');
         FlxG.mouse.load(sprite.pixels, 0.75, -20, -10);
+        FlxG.sound.muteKeys = null;
+        FlxG.sound.volumeUpKeys = null;
+        FlxG.sound.volumeDownKeys = null;
 	}
 }
